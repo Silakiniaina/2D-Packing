22ead865eea67227583f64460c72c435184ebb71
Silakiniaina <sandasilakiniaina4@gmail.com>
2024-07-17 12:17:00 +0300

    src : creation function pack in NFDH

--- src/packing2D/NFDH.java
+++ src/packing2D/NFDH.java
@@ -1,5 +1,40 @@
 package packing2D;
 
+import java.util.List;
+import java.util.ArrayList;
+import java.util.Comparator;
+
 public class NFDH {
 
+    /* ------------------------- Algo to pack using NFDH ------------------------ */
+    public static List<Rectangle> pack(List<Rectangle> rectangles, double containerWidth, double containerHeight) {
+        List<Rectangle> packedRectangles = new ArrayList<>();
+        
+        // Sort rectangles by height in descending order
+        rectangles.sort(Comparator.comparing(Rectangle::getHeight).reversed());
+        
+        double currentX = 0;
+        double currentY = 0;
+        double levelHeight = 0;
+
+        for (Rectangle rect : rectangles) {
+            if (currentX + rect.getWidth() > containerWidth) {
+                currentX = 0;
+                currentY += levelHeight;
+                levelHeight = 0;
+            }
+
+            if (currentY + rect.getHeight() > containerHeight) {
+                break;  // Container is full
+            }
+
+            rect.setPosition(currentX, currentY);
+            packedRectangles.add(rect);
+
+            currentX += rect.getWidth();
+            levelHeight = Math.max(levelHeight, rect.getHeight());
+        }
+
+        return packedRectangles;
+    }
 }
