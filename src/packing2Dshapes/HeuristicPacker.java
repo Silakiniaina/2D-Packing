be8a9e6cb43551ec00df33a01bb02a929e07650f
Silakiniaina <sandasilakiniaina4@gmail.com>
2024-07-17 12:44:37 +0300

    src : creation class HeuristicPacker

---
+++ src/packing2Dshapes/HeuristicPacker.java
@@ -0,0 +1,16 @@
+// File: src/packing2DShapes/HeuristicPacker.java
+package packing2Dshapes;
+
+import java.util.ArrayList;
+import java.util.Comparator;
+import java.util.List;
+
+public class HeuristicPacker {
+    private double containerWidth;
+    private double containerHeight;
+
+    public HeuristicPacker(double containerWidth, double containerHeight) {
+        this.containerWidth = containerWidth;
+        this.containerHeight = containerHeight;
+    }
+}
