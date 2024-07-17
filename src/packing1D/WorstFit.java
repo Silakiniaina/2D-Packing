6bd9e517a9cc84ba94c20d8f0ffd64dfb480b62b
Silakiniaina <sandasilakiniaina4@gmail.com>
2024-07-17 11:39:31 +0300

    src : creation function pack on WorstFit

--- src/packing1D/WorstFit.java
+++ src/packing1D/WorstFit.java
@@ -1,5 +1,33 @@
 package packing1D;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class WorstFit {
-    
+
+    /* --------- Function to pack a list of item using the worstFit algo -------- */
+    public static List<Bin> pack(List<Item> items, double binCapacity) {
+        List<Bin> bins = new ArrayList<>();
+
+        for (Item item : items) {
+            Bin worstBin = null;
+            double maxRemainingSpace = -1;
+
+            for (Bin bin : bins) {
+                if (bin.getRemainingSpace() >= item.getSize() && bin.getRemainingSpace() > maxRemainingSpace) {
+                    worstBin = bin;
+                    maxRemainingSpace = bin.getRemainingSpace();
+                }
+            }
+
+            if (worstBin == null) {
+                worstBin = new Bin(binCapacity);
+                bins.add(worstBin);
+            }
+
+            worstBin.addItem(item);
+        }
+
+        return bins;
+    }
 }
