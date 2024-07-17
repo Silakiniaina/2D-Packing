001c25c1f4f6cd06f31f9b38e576acff2e71040b
Silakiniaina <sandasilakiniaina4@gmail.com>
2024-07-17 12:20:17 +0300

    src : creation function pack in bestFit2d

--- src/packing2D/BestFit2D.java
+++ src/packing2D/BestFit2D.java
@@ -1,5 +1,48 @@
 package packing2D;
 
-public class BestFit2D {
+import java.util.List;
+import java.util.ArrayList;
 
+public class BestFit2D {
+    
+    /* ------------------ packing using the bestFit of 2d algo ------------------ */
+    public static List<Rectangle> pack(List<Rectangle> rectangles, double containerWidth, double containerHeight) {
+        List<Rectangle> packedRectangles = new ArrayList<>();
+        List<Rectangle> gaps = new ArrayList<>();
+        gaps.add(new Rectangle(containerWidth, containerHeight));
+
+        for (Rectangle rect : rectangles) {
+            Rectangle bestGap = null;
+            int bestGapIndex = -1;
+            double minWastedSpace = Double.MAX_VALUE;
+
+            for (int i = 0; i < gaps.size(); i++) {
+                Rectangle gap = gaps.get(i);
+                if (rect.getWidth() <= gap.getWidth() && rect.getHeight() <= gap.getHeight()) {
+                    double wastedSpace = (gap.getWidth() * gap.getHeight()) - rect.getArea();
+                    if (wastedSpace < minWastedSpace) {
+                        bestGap = gap;
+                        bestGapIndex = i;
+                        minWastedSpace = wastedSpace;
+                    }
+                }
+            }
+
+            if (bestGap != null) {
+                rect.setPosition(bestGap.getX(), bestGap.getY());
+                packedRectangles.add(rect);
+                gaps.remove(bestGapIndex);
+
+                // Create new gaps
+                if (bestGap.getWidth() > rect.getWidth()) {
+                    gaps.add(new Rectangle(bestGap.getWidth() - rect.getWidth(), rect.getHeight()));
+                }
+                if (bestGap.getHeight() > rect.getHeight()) {
+                    gaps.add(new Rectangle(bestGap.getWidth(), bestGap.getHeight() - rect.getHeight()));
+                }
+            }
+        }
+
+        return packedRectangles;
+    }
 }
