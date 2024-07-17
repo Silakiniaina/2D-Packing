5f95ec8ccdda3de29af1debc85cd904570c54abc
Silakiniaina <sandasilakiniaina4@gmail.com>
2024-07-17 12:45:05 +0300

    src : creation function pack in HeuristiPacker

--- src/packing2Dshapes/HeuristicPacker.java
+++ src/packing2Dshapes/HeuristicPacker.java
@@ -13,4 +13,45 @@ public class HeuristicPacker {
         this.containerWidth = containerWidth;
         this.containerHeight = containerHeight;
     }
+
+    public List<Shape> pack(List<Shape> shapes) {
+        List<Shape> packedShapes = new ArrayList<>();
+        shapes.sort(Comparator.comparing(Shape::getArea).reversed());
+
+        double currentX = 0;
+        double currentY = 0;
+        double rowHeight = 0;
+
+        for (Shape shape : shapes) {
+            boolean placed = false;
+            for (double rotation : new double[]{0, Math.PI/2, Math.PI}) {
+                shape.rotate(rotation);
+                if (currentX + shape.getWidth() <= containerWidth &&
+                    currentY + shape.getHeight() <= containerHeight) {
+                    shape.setPosition(currentX, currentY);
+                    packedShapes.add(shape.clone());
+                    currentX += shape.getWidth();
+                    rowHeight = Math.max(rowHeight, shape.getHeight());
+                    placed = true;
+                    break;
+                }
+            }
+
+            if (!placed) {
+                currentX = 0;
+                currentY += rowHeight;
+                rowHeight = 0;
+                if (currentY + shape.getHeight() <= containerHeight) {
+                    shape.setPosition(currentX, currentY);
+                    packedShapes.add(shape.clone());
+                    currentX += shape.getWidth();
+                    rowHeight = shape.getHeight();
+                } else {
+                    break; // Container is full
+                }
+            }
+        }
+
+        return packedShapes;
+    }
 }
